8805ee77ffd99fb18d427f0076c18cde01f7316f
nessakiani <nessakiani@gmail.com>
2023-04-10 23:24:14 -0700

    parallel data loading

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -6,13 +6,15 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: implement this
+time echo "$files" | parallel ./load_denormalized.sh
+
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2/ --inputs
+time echo "$files" | parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2280/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs
+time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3280/ --inputs
