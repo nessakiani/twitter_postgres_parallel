7e6b28c59bb5ce988e34950ddf716ad59d52de45
nessakiani <nessakiani@gmail.com>
2023-04-11 10:39:15 -0700

    fixed permissions and time

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -6,15 +6,15 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: implement this
-time echo "$files" | parallel ./load_denormalized.sh
+echo "$files" | time parallel ./load_denormalized.sh
 
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time echo "$files" | parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2280/ --inputs
+echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2280/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3280/ --inputs
+echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3280/ --inputs
