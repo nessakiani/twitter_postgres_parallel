c1973d853d85a9cdd6dd989084c0fc4ac6bde9df
nessakiani <nessakiani@gmail.com>
2023-04-11 23:17:36 -0700

    yay

--- services/pg_normalized_batch/schema.sql
+++ services/pg_normalized_batch/schema.sql
@@ -4,22 +4,23 @@ CREATE EXTENSION postgis;
 
 BEGIN;
 
+
 /*
  * Users may be partially hydrated with only a name/screen_name 
  * if they are first encountered during a quote/reply/mention 
  * inside of a tweet someone else's tweet.
  */
 CREATE TABLE users (
-    id_users BIGINT,
+    id_users BIGINT,-- PRIMARY KEY,
     created_at TIMESTAMPTZ,
     updated_at TIMESTAMPTZ,
+    url TEXT, 
     friends_count INTEGER,
     listed_count INTEGER,
     favourites_count INTEGER,
     statuses_count INTEGER,
     protected BOOLEAN,
     verified BOOLEAN,
-    url TEXT,
     screen_name TEXT,
     name TEXT,
     location TEXT,
@@ -31,7 +32,7 @@ CREATE TABLE users (
  * Tweets may be entered in hydrated or unhydrated form.
  */
 CREATE TABLE tweets (
-    id_tweets BIGINT,
+    id_tweets BIGINT,-- PRIMARY KEY,
     id_users BIGINT,
     created_at TIMESTAMPTZ,
     in_reply_to_status_id BIGINT,
@@ -56,31 +57,35 @@ CREATE TABLE tweets (
     -- FOREIGN KEY (in_reply_to_status_id) REFERENCES tweets(id_tweets),
     -- FOREIGN KEY (quoted_status_id) REFERENCES tweets(id_tweets)
 );
-
 CREATE INDEX tweets_index_geo ON tweets USING gist(geo);
-
 CREATE INDEX tweets_index_withheldincountries ON tweets USING gin(withheld_in_countries);
 
 CREATE TABLE tweet_urls (
     id_tweets BIGINT,
     url TEXT
+    --id_urls
+   -- PRIMARY KEY (id_tweets, id_urls),
+    --FOREIGN KEY (id_tweets) REFERENCES tweets(id_tweets) DEFERRABLE INITIALLY DEFERRED,
+    --FOREIGN KEY (id_urls) REFERENCES urls(id_urls) DEFERRABLE INITIALLY DEFERRED
 );
 
 
 CREATE TABLE tweet_mentions (
     id_tweets BIGINT,
     id_users BIGINT
+    --PRIMARY KEY (id_tweets, id_users),
+    --FOREIGN KEY (id_tweets) REFERENCES tweets(id_tweets) DEFERRABLE INITIALLY DEFERRED,
+    --FOREIGN KEY (id_users) REFERENCES users(id_users) DEFERRABLE INITIALLY DEFERRED
 );
-
 CREATE INDEX tweet_mentions_index ON tweet_mentions(id_users);
 
 CREATE TABLE tweet_tags (
     id_tweets BIGINT,
     tag TEXT
+    --PRIMARY KEY (id_tweets, tag),
+    --FOREIGN KEY (id_tweets) REFERENCES tweets(id_tweets) DEFERRABLE INITIALLY DEFERRED
 );
-
 COMMENT ON TABLE tweet_tags IS 'This table links both hashtags and cashtags';
-
 CREATE INDEX tweet_tags_index ON tweet_tags(id_tweets);
 
 
@@ -88,6 +93,10 @@ CREATE TABLE tweet_media (
     id_tweets BIGINT,
     url TEXT,
     type TEXT
+    --id_urls
+    --PRIMARY KEY (id_tweets, id_urls),
+    --FOREIGN KEY (id_urls) REFERENCES urls(id_urls) DEFERRABLE INITIALLY DEFERRED,
+    --FOREIGN KEY (id_tweets) REFERENCES tweets(id_tweets) DEFERRABLE INITIALLY DEFERRED
 );
 
 /*
